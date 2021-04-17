b5e2fbdcbfbb0f14745fd7d46c7948fc81ab7759
Jose L. Lozano <jllozano.mx@gmail.com>
2021-04-17 16:05:34 -0500

    Add Tree class

--- bst.rb
+++ bst.rb
@@ -1,10 +1,9 @@
-# class that allows us bst node creation
 class Node
   include Comparable
 
   attr_accessor :data, :left, :right
 
-  def initialize(data, left, right)
+  def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
@@ -12,5 +11,25 @@ class Node
 
   def <=>(other)
     data <=> other.data
+  end
+end
+
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @root = build_tree(array.sort.uniq)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+    return Node.new(array[0]) if array.size == 1
+
+    mid_index = array.size / 2
+    data = array[mid_index]
+    left_subtree = array[0..(mid_index - 1)]
+    right_subtree = array[(mid_index + 1)..-1]
+
+    Node.new(data, build_tree(left_subtree), build_tree(right_subtree))
   end
 end

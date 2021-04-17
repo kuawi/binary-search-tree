463c20bb7e15ab391eb1d5789cc978c42d1b3975
Jose L. Lozano <jllozano.mx@gmail.com>
2021-04-17 14:40:00 -0500

    Add Node class

---
+++ bst.rb
@@ -0,0 +1,16 @@
+# class that allows us bst node creation
+class Node
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data, left, right)
+    @data = data
+    @left = left
+    @right = right
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
+end
